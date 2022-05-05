b97a2fad0a80f680c02d27e7b0295e28f5c2b8e7
Kchanghun <abcd877287@gmail.com>
2022-05-06 08:21:12 +0900

    modify queryset using pk(dog_id)

--- pupple_backend/pass_condition/views.py
+++ pupple_backend/pass_condition/views.py
@@ -7,3 +7,8 @@ from .models import PassCondition
 class PassConditionViewSet(viewsets.ModelViewSet):
     queryset = PassCondition.objects.all().order_by('dog_id')
     serializer_class = PassConditionSerializer
+    
+    def get_queryset(self):
+        queryset = PassCondition.objects.all()
+        dog_id = self.kwargs['dog_id']
+        return queryset.filter(dog_id = dog_id)

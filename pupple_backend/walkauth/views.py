e30d28972fdf6f45b95037ed6dc1a4e801c6b1e6
Kchanghun <abcd877287@gmail.com>
2022-05-06 06:11:21 +0900

    modify comment

--- pupple_backend/walkauth/views.py
+++ pupple_backend/walkauth/views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets
 from .serializers import WalkAuthSerializer
 from .models import WalkAuth
 
-import time
 # Create your views here.
 
 class WalkAuthViewSet(viewsets.ModelViewSet):
@@ -14,7 +13,7 @@ class WalkAuthViewSet(viewsets.ModelViewSet):
         queryset = WalkAuth.objects.all()
         userdog = self.kwargs['userdog']
         return queryset.filter(userdog=userdog).order_by('day')
-        # 구매자와 강아지 아이디를 통해 query 구성
+        # 구매자와 강아지 아이디를 통해 pk인 userdog 사용
 
     
 

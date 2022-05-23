4ffab590f23a525c5a88eeeea56d630e564c8a57
JEwon98 <rhojewon@gmail.com>
2022-05-23 21:19:21 +0900

    0523

--- pupple_backend/house_photo/urls.py
+++ pupple_backend/house_photo/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('get/',getphoto_view,name='get_housephoto'),
     path('get/mydog/',get_all_mydog_photo_view,name='get_all_mydog_photo_view'), # 판매자 email만 입력받으면 판매자가 가진 모든 강아지에게 온 사진데이터 전송
     path('get/mydog/onlyone/',get_one_dog_photo_view,name='get_one_dog_photo_view'), # 강아지 아이디를 입력 받으면 해당 강아지아이디로 온 모든 사진데이터 전송
+
+
 ]

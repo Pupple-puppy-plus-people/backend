1eb5ede27371fd3ee5d43df2b19a2b7ca6e315db
Kchanghun <abcd877287@gmail.com>
2022-06-02 18:00:54 +0900

    change redis host

--- pupple_backend/pupple_backend/settings.py
+++ pupple_backend/pupple_backend/settings.py
@@ -166,7 +166,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('172.31.40.247', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
